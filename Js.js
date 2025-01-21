954ac52be2f96eb3fe1e8ac3153f831703b7ce2e
theking787899 <gustavo.roben777@gmail.com>
2025-01-21 15:06:59 -0300

    Delete Js.js

--- Js.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById('menu');
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  }
-}
-
